6957a658ad29eb9d62094ddc4790922f6d630990
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 22:30:17 +0300

    Add files via upload

---
+++ label büyültme küçülme/label büyültme küçülme/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        int i = 8;
+        int a = 10;
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            label1.Font = new Font("",i);
+            i++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Font = new Font("", i);
+            i--;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            label2.Font = new Font("", a);
+            a++;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ALLAHIN İSMİ ASLA KÜÇÜLMEZ", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            
+        }
+
+       
+        }
+    }
+
